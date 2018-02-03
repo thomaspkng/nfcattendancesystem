a7203e52705369fe5178eb3b8945f1b561acd7e1
TN <thomas.supertunnel@gmail.com>
2018-02-04 02:11:55 +0800

    copy js's to root directory

---
+++ nfcsystem.js
@@ -0,0 +1,35 @@
+document.addEventListener("deviceready", onDeviceReady, msgfail);
+function onDeviceReady() {
+        // Now safe to use the PhoneGap API
+		alert("Check listener!");
+		readtagid();
+}
+function msgfail() {
+	alert("deviceready Fail");
+	}
+function tagidread1() {
+	alert("listener add callback");
+	}
+function tagidread() {
+	alert("tag id read");
+	}
+function onSuccess() {
+	alert("nfc listener added");
+	}
+function onFailure(msg) {
+	alert(msg);
+	}
+function readtagid() {
+	alert("enter readtagid()");
+	nfc.addTagDiscoveredListener(tagidread, onSuccess, onFailure);
+	}
+$(document).ready(function() {
+    //alert("doc ready");
+/*	
+	nfc.enabled(
+  function() {alert('yes!')},
+  // msg is one of NO_NFC (no hardware support) or NFC_DISABLED (supported but disabled)
+  function(msg) {alert(msg)}
+);
+*/
+});
