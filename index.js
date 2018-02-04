b4757474bf76c5cc51de921e1d1c647c4e1b7feb
TN <thomas.supertunnel@gmail.com>
2018-02-04 22:55:18 +0800

    add duuid()

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ function takein() {
 	$("#state123").val("1");
 	$("#inorout").html("IN");
 	}
-
 function takeout() {
 	$("#state123").val("2");
 	$("#inorout").html("OUT");
 	}
-
 function otout() {
 	$("#state123").val("3");
 	$("#inorout").html("OT");
 	}
-
 function inoutot() {
 	alert("inoutot, OK");
 	$("#checkin").click(takein);
 	$("#checkout").click(takeout);
 	$("#checkotout").click(otout);
+	}
+function duuid() {
+	var deviceuuid = device.uuid;
+	$("#duuid").html(deviceuuid);
 	}
