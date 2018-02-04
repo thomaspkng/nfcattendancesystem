65b689ad071c947e9bb48631ca2c649aea5ba0d6
TN <thomas.supertunnel@gmail.com>
2018-02-04 23:00:12 +0800

    revise output data

--- gpssystem.js
+++ gpssystem.js
@@ -1,25 +1,14 @@
 function readGPS() {
-//	document.getElementById("geolocation").innerHTML = "readGPS OK!";
 	navigator.geolocation.getCurrentPosition(gpsSuccess, gpsError);
 }
-
 // onSuccess Geolocation
 //
 function gpsSuccess(position) {
 	var element = document.getElementById('geolocation');
-	element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-						'Longitude: '          + position.coords.longitude             + '<br />' +
-						'Altitude: '           + position.coords.altitude              + '<br />' +
-						'Accuracy: '           + position.coords.accuracy              + '<br />' +
-						'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-						'Heading: '            + position.coords.heading               + '<br />' +
-						'Speed: '              + position.coords.speed                 + '<br />' +
-						'Timestamp: '          + new Date(position.timestamp)          + '<br />';
+	element.innerHTML = 'N: ' + position.coords.latitude + '<br />' + 'E: ' + position.coords.longitude + '<br />';
 }
-
 // onError Callback receives a [PositionError](PositionError/positionError.html) object
 //
 function gpsError(error) {
-	alert('code: '    + error.code    + '\n' +
-		  'message: ' + error.message + '\n');
+	alert('code: ' + error.code    + '\n' + 'message: ' + error.message + '\n');
 }
