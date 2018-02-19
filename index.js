cdbfcc809d9103d4075722b3b00548cfcfb8b0cd
TN <thomas.supertunnel@gmail.com>
2018-02-20 01:45:19 +0800

    add Btn function and revised name
    add function noononstop() and revise big btn contect by adding chinese words

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ function quitapp() {
 	}
 function takein() {
 	$("#s_state123").val("1");
-	$("#inorout").html("IN");
+	$("#inorout").html("IN<br />上班");
 	}
 function takeout() {
 	$("#s_state123").val("2");
-	$("#inorout").html("OUT");
+	$("#inorout").html("OUT<br />下班");
 	}
 function otout() {
 	$("#s_state123").val("3");
-	$("#inorout").html("OT");
+	$("#inorout").html("OT-OUT<br />超時下班");
+	}
+function noononstop() {
+	$("#s_state123").val("4");
+	$("#inorout").html("Noon<br />中直");
 	}
 function inoutot() {
 //	alert("inoutot, OK");
 	$("#checkin").click(takein);
 	$("#checkout").click(takeout);
 	$("#checkotout").click(otout);
+	$("#noononstop").click(noononstop);
 	}
 function duuid() {
 	var deviceuuid = device.uuid;
@@ -32,7 +37,6 @@ function tag2server() {
 	qs_4 = "gpse="+$("#gpse").val();
 	qs_5 = "state123="+$("#s_state123").val();
 	url2go = url_server + qs_1 + "&" + qs_2 + "&" + qs_3 + "&" + qs_4 + "&" + qs_5;
-//	alert(url2go);
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
